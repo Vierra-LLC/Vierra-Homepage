14d53a5666ac49664228d72e74090907db82b815
Kjazer004 <98173440+Kjazer004@users.noreply.github.com>
2023-11-19 18:39:35 +0100

    Another commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+import Hero from "@/components/elements/Hero";
 import React from "react";
 
 function page() {
-  return <div>Coming soon</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
 
 export default page;

85d2ed0376ffabae95fda9939a8c6ff93990ca4c
Kjazer004 <98173440+Kjazer004@users.noreply.github.com>
2023-11-29 17:14:29 +0100

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Alexersion | Home",
+  title: "Vierra | Home",
   description:
     "Greetings, my name is Alex and I am a multi-skilled entrepreneur. I am dedicated to providing the highest quality products and services. I strive to stay at the forefront of my industry, constantly seeking out new opportunities and advancements. Please visit my links to learn more about me, my shop, and my current projects.",
   author: "Alexersion",
@@ -12,9 +12,9 @@ export const metadata = {
     domain_verify: "b5b96b6ba822cdff32f61e172488f254",
   },
   openGraph: {
-    url: "https://alexersion.com",
+    url: "https://vierradev.com",
     type: "website",
-    siteName: "Alexersion",
+    siteName: "Vierra",
     images: [
       "https://firebasestorage.googleapis.com/v0/b/picture-host.appspot.com/o/meta-banner.png?alt=media&token=72c91394-aa3f-4104-92bd-6ef961be964c",
     ],
@@ -22,9 +22,9 @@ export const metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    domain: "alexersion.com",
-    url: "alexersion.com",
-    title: "Alexersion | Home",
+    domain: "vierradev.com",
+    url: "vierradev.com",
+    title: "Vierra | Home",
     description:
       "Greetings, my name is Alex and I am a multi-skilled entrepreneur. I am dedicated to providing the highest quality products and services. I strive to stay at the forefront of my industry, constantly seeking out new opportunities and advancements. Please visit my links to learn more about me, my shop, and my current projects.",
     images: [

47373bbdf021b6bd7b3a3db529f381a94990cfd3
Alexersion <alexersion@gmail.com>
2023-12-28 09:17:18 -0500

    Updated the favicon and meta data information.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,42 +1,44 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Vierra | Home",
+  color: "#BA2A99",
   description:
-    "Vierra is a versatile company dedicated to delivering comprehensive software solutions tailored for medical practices. Specifically designed to cater to small business owners, Vierra offers a seamless package encompassing CRM and marketing/sales tools for these practices. Beyond that, Vierra's software suite extends to cover an array of functionalities, including 3D modeling, task management, insurance administration, customer interactions, patient history, and financial management, which includes integration with QuickBooks. Our software is accessible on both mobile and desktop platforms, with an online web portal connecting to an e-commerce store for medical products and materials. Additionally, Vierra operates on a subscription-based model, allowing us to work business-by-business, offering individualized support for each client's company growth aspirations. Say goodbye to fragmented divisions—Vierra presents an all-in-one software and consulting solution to streamline operations, from accounting to sales, and more.",
+    "Our comprehensive tool offers small business owners a one-stop-shop solution to manage their accounting, sales, marketing, outreach efforts, CRM, financial models, email campaigns, QuickBooks integration, record-keeping, and a range of other crucial business functions.",
   author: "Alexersion",
   p: {
     domain_verify: "b5b96b6ba822cdff32f61e172488f254",
   },
-  openGraph: {
-    url: "https://vierradev.com",
-    type: "website",
-    siteName: "Vierra",
-    images: [
-      "https://firebasestorage.googleapis.com/v0/b/picture-host.appspot.com/o/vierra_banner.png?alt=media&token=c9be886a-9e51-48ba-b1c7-8c94ff42d008",
-    ],
-    locale: "en_US",
-  },
-  twitter: {
-    card: "summary_large_image",
-    domain: "vierradev.com",
-    url: "vierradev.com",
-    title: "Vierra | Home",
-    description:
-      "Vierra is a versatile company dedicated to delivering comprehensive software solutions tailored for medical practices. Specifically designed to cater to small business owners, Vierra offers a seamless package encompassing CRM and marketing/sales tools for these practices. Beyond that, Vierra's software suite extends to cover an array of functionalities, including 3D modeling, task management, insurance administration, customer interactions, patient history, and financial management, which includes integration with QuickBooks. Our software is accessible on both mobile and desktop platforms, with an online web portal connecting to an e-commerce store for medical products and materials. Additionally, Vierra operates on a subscription-based model, allowing us to work business-by-business, offering individualized support for each client's company growth aspirations. Say goodbye to fragmented divisions—Vierra presents an all-in-one software and consulting solution to streamline operations, from accounting to sales, and more.",
-    images: [
-      "https://firebasestorage.googleapis.com/v0/b/picture-host.appspot.com/o/vierra_banner.png?alt=media&token=c9be886a-9e51-48ba-b1c7-8c94ff42d008",
-    ],
-  },
-};
+  // openGraph: {
+  //   url: "https://vierradev.com",
+  //   type: "website",
+  //   siteName: "Vierra",
+  //   images: [
+  //     "https://firebasestorage.googleapis.com/v0/b/picture-host.appspot.com/o/vierra_banner.png?alt=media&token=c9be886a-9e51-48ba-b1c7-8c94ff42d008",
+  //   ],
+  //   locale: "en_US",
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   domain: "vierradev.com",
+  //   url: "vierradev.com",
+  //   title: "Vierra | Home",
+  //   color: "#BA2A99",
+  //   description:
+  //     "Our comprehensive tool offers small business owners a one-stop-shop solution to manage their accounting, sales, marketing, outreach efforts, CRM, financial models, email campaigns, QuickBooks integration, record-keeping, and a range of other crucial business functions.",
+  //   images: [
+  //     "https://firebasestorage.googleapis.com/v0/b/picture-host.appspot.com/o/vierra_banner.png?alt=media&token=c9be886a-9e51-48ba-b1c7-8c94ff42d008",
+  //   ],
+  // },
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }

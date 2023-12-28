d39bb597c3a5e4ebe91c73c2649d3c6190c8d741
Alexersion <alexersion@gmail.com>
2023-12-28 09:56:05 -0500

    Updated the favicon in the meta data and added a banner.

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,27 +12,23 @@ export const metadata = {
   p: {
     domain_verify: "b5b96b6ba822cdff32f61e172488f254",
   },
-  // openGraph: {
-  //   url: "https://vierradev.com",
-  //   type: "website",
-  //   siteName: "Vierra",
-  //   images: [
-  //     "https://firebasestorage.googleapis.com/v0/b/picture-host.appspot.com/o/vierra_banner.png?alt=media&token=c9be886a-9e51-48ba-b1c7-8c94ff42d008",
-  //   ],
-  //   locale: "en_US",
-  // },
-  // twitter: {
-  //   card: "summary_large_image",
-  //   domain: "vierradev.com",
-  //   url: "vierradev.com",
-  //   title: "Vierra | Home",
-  //   color: "#BA2A99",
-  //   description:
-  //     "Our comprehensive tool offers small business owners a one-stop-shop solution to manage their accounting, sales, marketing, outreach efforts, CRM, financial models, email campaigns, QuickBooks integration, record-keeping, and a range of other crucial business functions.",
-  //   images: [
-  //     "https://firebasestorage.googleapis.com/v0/b/picture-host.appspot.com/o/vierra_banner.png?alt=media&token=c9be886a-9e51-48ba-b1c7-8c94ff42d008",
-  //   ],
-  // },
+  openGraph: {
+    url: "https://vierradev.com",
+    type: "website",
+    siteName: "Vierra",
+    images: ["https://vierradev.com/assets/meta-banner"],
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    domain: "vierradev.com",
+    url: "vierradev.com",
+    title: "Vierra | Home",
+    color: "#BA2A99",
+    description:
+      "Our comprehensive tool offers small business owners a one-stop-shop solution to manage their accounting, sales, marketing, outreach efforts, CRM, financial models, email campaigns, QuickBooks integration, record-keeping, and a range of other crucial business functions.",
+    images: ["https://vierradev.com/assets/meta-banner"],
+  },
 }
 
 export default function RootLayout({ children }) {

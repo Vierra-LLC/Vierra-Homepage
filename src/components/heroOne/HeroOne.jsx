974485e3cf54a6cd8eddb607c68f4bca593f6ffb
Sodiumtelluride <gelfandnat@gmail.com>
2025-02-06 22:09:35 -0500

    Finished Hero One

--- src/components/heroOne/HeroOne.jsx
+++ src/components/heroOne/HeroOne.jsx
@@ -4,6 +4,7 @@ import LightLargeBtn from '../lightLargeBtn/LightLargeBtn';
 import DarkLargeBtn from '../darkLargeBtn/DarkLargeBtn'
 import ellipse from '../../assets/ellipse.svg';
 import phones from '../../assets/phones.png';
+import Logoipsum from '../../assets/Logoipsum.svg';
 const HeroOne = () => {
     return (
         <div className="hero-one-contianer">
@@ -11,16 +12,22 @@ const HeroOne = () => {
                 <img className="ellipse-one" src={ellipse} alt="" />
                 <img className="ellipse-two" src={ellipse} alt="" />
             </div>
-            <div className = "hero-one-content">
+            <div className = "main-content">
                 <div className="section-one">
                     <h1 className="hero-one-tagline">We boost your brand to thrive in the digital world.</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-                    <div className="grid">
+                    <div className="booking-grid">
                         <DarkLargeBtn className="book-button" text="Book a meeting" onClick={() => alert('Hello, world!')} />
                         <a href="/">Our Solutions</a>
                     </div>
                 </div>
                 <img src={phones} alt="" className="phones-image" />
+            </div>
+            <div className="logos-grid">
+                <p>Trusted by our partners</p>
+                <img className="logo" src={Logoipsum} alt="" />
+                <img className="logo" src={Logoipsum} alt="" />
+                <img className="logo" src={Logoipsum} alt="" />
             </div>
         </div>
     );
